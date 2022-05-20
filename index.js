3b64ceee23d144ba314935f2a5e06d58abd89b70
alice benatti <74602443+ali-benny@users.noreply.github.com>
2022-05-20 13:08:46 +0200

    Fixed accents bug

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ function course(msg, name, virtuale, teams, website, professors) {
   const toOurCase = str =>
     str &&
     str
+      .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
       .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
       .map(x => x.toLowerCase())
       .join('-');
