45213f80eb6cd9e95fcc74abbef3d936ef4a8a80
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-03-19 18:57:30 +0100

    refactor(bot): add meme commands and refactor bot handling commands

--- bot/bot.go
+++ bot/bot.go
@@ -12,6 +12,8 @@ import (
 var (
 	autoreplies []model.AutoReply
 	actions     []model.Action
+	settings    model.Settings
+	memeList    []model.Meme
 )
 
 func StartInformaBot(token string, debug bool) {
@@ -33,6 +35,16 @@ func StartInformaBot(token string, debug bool) {
 		log.Fatalf("Error reading actions.json file: %s", err.Error())
 	}
 
+	settings, err = parse.ParseSettings()
+	if err != nil {
+		log.Fatalf("Error reading settings.json file: %s", err.Error())
+	}
+
+	memeList, err = parse.ParseMemeList()
+	if err != nil {
+		log.Fatalf("Error reading memes.json file: %s", err.Error())
+	}
+
 	run(bot)
 }
 
@@ -52,27 +64,7 @@ func run(bot *tgbotapi.BotAPI) {
 		}
 
 		if update.Message.IsCommand() {
-			commandName := strings.ToLower(update.Message.Command())
-
-			comparator := func(action model.Action, commandName string) bool {
-				return action.Name == commandName
-			}
-			index := Find(actions, commandName, comparator)
-			if index != -1 {
-				log.Printf("@%s: \t%s -> %s", update.Message.From.UserName, update.Message.Text, commandName)
-				commandName = actions[index].Name
-				for commandName != "" {
-					commandName = actions[index].Data.HandleBotCommand(bot, update.Message)
-					index = Find(actions, commandName, comparator)
-				}
-
-				// NOTA: un pattern di questo genere ha senso?
-				// invece di chiamare direttamente il metodo su Data, ci teniamo un passaggio di mezzo
-				// come se fosse middleware, per cose come log.
-				// actions[index].Execute(bot, update)
-			} else {
-				log.Printf("@%s: \t%s -> COMMAND NOT AVAILABLE", update.Message.From.UserName, update.Message.Text)
-			}
+			handleCommand(bot, &update)
 		} else {
 			// text message
 			for i := 0; i < len(autoreplies); i++ {
@@ -85,6 +77,55 @@ func run(bot *tgbotapi.BotAPI) {
 		}
 
 	}
+}
+
+func handleCommand(bot *tgbotapi.BotAPI, update *tgbotapi.Update) {
+	commandName := strings.ToLower(update.Message.Command())
+
+	hasExecutedCommand := executeCommandWithName(bot, update, commandName)
+	if !hasExecutedCommand {
+		memeIndex := Find(memeList, commandName, func(meme model.Meme, commandName string) bool {
+			return meme.Name == commandName
+		})
+
+		if memeIndex != -1 {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, memeList[memeIndex].Text)
+			bot.Send(msg)
+		} else {
+			executeCommandWithName(bot, update, "unknown")
+			log.Printf("@%s: \t%s -> COMMAND NOT AVAILABLE", update.Message.From.UserName, update.Message.Text)
+		}
+	}
+}
+
+// executes a given command in the command list, given its index
+// if invalid index, does nothing
+func executeCommand(bot *tgbotapi.BotAPI, update *tgbotapi.Update, commandIndex int) {
+	if commandIndex >= 0 && commandIndex < len(actions) {
+		newCommand := actions[commandIndex].Data.HandleBotCommand(bot, update.Message)
+		if newCommand != "" {
+			// NOTA: un pattern di questo genere ha senso?
+			// invece di chiamare direttamente il metodo su Data, ci teniamo un passaggio di mezzo
+			// come se fosse middleware, per cose come log.
+			// actions[index].Execute(bot, update)
+			executeCommandWithName(bot, update, newCommand)
+		}
+	}
+}
+
+// executes a given command in the command list, given its name
+// @return true if command was found, false otherwise
+func executeCommandWithName(bot *tgbotapi.BotAPI, update *tgbotapi.Update, commandName string) bool {
+	idx := Find(actions, commandName, func(action model.Action, commandName string) bool {
+		return action.Name == commandName
+	})
+
+	if idx != -1 {
+		executeCommand(bot, update, idx)
+		return true
+	}
+
+	return false
 }
 
 func Find[T any, Q any](a []T, x Q, compare func(T, Q) bool) int {
