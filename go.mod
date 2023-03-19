e697986dc9d9794824225a62ce409efb8c2cb0b1
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-03-19 15:03:38 +0100

    test(util): test the kebab case function

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
---
+++ model/util_test.go
@@ -0,0 +1,55 @@
+package model
+
+import (
+	"testing"
+)
+
+func TestToKebabCase(t *testing.T) {
+	// Test strings
+	str1 := "Logica per l'informatica"
+	str2 := "Informatica e Societa"
+	str3 := "Sistemi Operativi"
+
+	// Expected results
+	exp1 := "logica-per-informatica"
+	exp2 := "informatica-e-societa"
+	exp3 := "sistemi-operativi"
+
+	// Test
+	res1 := ToKebabCase(str1)
+	res2 := ToKebabCase(str2)
+	res3 := ToKebabCase(str3)
+
+	// Check results
+
+	if res1 != exp1 {
+		t.Errorf("Expected %s, got %s", exp1, res1)
+	}
+
+	if res2 != exp2 {
+		t.Errorf("Expected %s, got %s", exp2, res2)
+	}
+
+	if res3 != exp3 {
+		t.Errorf("Expected %s, got %s", exp3, res3)
+	}
+}
+
+func TestAccents(t *testing.T) {
+	str := "Informatica e Società"
+	exp := "informatica-e-societa"
+
+	str2 := "à è ì ò ù"
+	exp2 := "a-e-i-o-u"
+
+	res := ToKebabCase(str)
+	res2 := ToKebabCase(str2)
+
+	if res != exp {
+		t.Errorf("Expected %s, got %s", exp, res)
+	}
+
+	if res2 != exp2 {
+		t.Errorf("Expected %s, got %s", exp2, res2)
+	}
+}
