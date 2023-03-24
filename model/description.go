b96f80e5ebb29b39ee9f15bf2b2cd654c8ecb22a
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-03-24 22:59:47 +0100

    feat(help): implement help command

---
+++ model/description.go
@@ -0,0 +1,45 @@
+package model
+
+func (d MessageData) GetDescription() string {
+	return d.Description
+}
+
+func (d HelpData) GetDescription() string {
+	return d.Description
+}
+
+func (d LookingForData) GetDescription() string {
+	return d.Description
+}
+
+func (d NotLookingForData) GetDescription() string {
+	return d.Description
+}
+
+func (d YearlyData) GetDescription() string {
+	return d.Description
+}
+
+func (d TodayLecturesData) GetDescription() string {
+	return d.Description
+}
+
+func (d TomorrowLecturesData) GetDescription() string {
+	return d.Description
+}
+
+func (d ListData) GetDescription() string {
+	return d.Description
+}
+
+func (d CourseData) GetDescription() string {
+	return d.Description
+}
+
+func (d LuckData) GetDescription() string {
+	return d.Description
+}
+
+func (d InvalidData) GetDescription() string {
+	return "This data is invalidly parsed, please report this bug to the developer."
+}
