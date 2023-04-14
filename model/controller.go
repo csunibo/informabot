85d5c443fdb7eed4d6b4b992e8b262d02868ee0e
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2023-04-14 20:17:06 +0200

    fix teachings

--- model/controller.go
+++ model/controller.go
@@ -166,6 +166,19 @@ func (data ListData) HandleBotCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Me
 	return makeResponseWithText(resultText)
 }
 
+const BEGINNING_MONTH = time.September
+
+func getCurrentAcademicYear() int {
+
+	now := time.Now()
+	year := now.Year()
+	if now.Month() >= BEGINNING_MONTH {
+		return year
+	} else {
+		return year - 1
+	}
+}
+
 func (data CourseData) HandleBotCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message) CommandResponse {
 	emails := strings.Join(data.Professors, "@unibo.it\n ") + "@unibo.it\n"
 	ternary_assignment := func(condition bool, true_value string) string {
@@ -176,14 +189,13 @@ func (data CourseData) HandleBotCommand(bot *tgbotapi.BotAPI, message *tgbotapi.
 		}
 	}
 
+	currentAcademicYear := fmt.Sprint(getCurrentAcademicYear())
 	msg := ternary_assignment(data.Name != "", fmt.Sprintf("<b>%s</b>\n", data.Name)) +
-		ternary_assignment(data.Virtuale != "", fmt.Sprintf("<a href='https://virtuale.unibo.it/course/view.php?id=%s'>Virtuale</a>", data.Virtuale)+"\n") +
-		ternary_assignment(data.Teams != "", fmt.Sprintf("<a href='https://teams.microsoft.com/l/meetup-join/19%%3ameeting_%s", data.Teams)+"%40thread.v2/0?context=%7b%22Tid%22%3a%22e99647dc-1b08-454a-bf8c-699181b389ab%22%2c%22Oid%22%3a%22080683d2-51aa-4842-aa73-291a43203f71%22%7d'>Videolezione</a>\n") +
-		ternary_assignment(data.Website != "", fmt.Sprintf("<a href='https://www.unibo.it/it/didattica/insegnamenti/insegnamento/%s'>Sito</a>\n<a href='https://www.unibo.it/it/didattica/insegnamenti/insegnamento/%s/orariolezioni'>Orario</a>", data.Website, data.Website)+"\n") +
+		ternary_assignment(data.Website != "", fmt.Sprintf("<a href='https://www.unibo.it/it/didattica/insegnamenti/insegnamento/%s/%s'>Sito</a>\n<a href='https://www.unibo.it/it/didattica/insegnamenti/insegnamento/%s/%s/orariolezioni'>Orario</a>", currentAcademicYear, data.Website, currentAcademicYear, data.Website)+"\n") +
 		ternary_assignment(data.Professors != nil, fmt.Sprintf("Professori:\n %s", emails)) +
 		ternary_assignment(data.Name != "", fmt.Sprintf("<a href='https://csunibo.github.io/%s/'>📚 Risorse: materiali, libri, prove</a>\n", utils.ToKebabCase(data.Name))) +
 		ternary_assignment(data.Name != "", fmt.Sprintf("<a href='https://github.com/csunibo/%s/'>📂 Repository GitHub delle risorse</a>\n", utils.ToKebabCase(data.Name))) +
-		ternary_assignment(data.Telegram != "", fmt.Sprintf("<a href='t.me/$%s'>👥 Gruppo Studenti</a>\n", data.Telegram))
+		ternary_assignment(data.Telegram != "", fmt.Sprintf("<a href='t.me/%s'>👥 Gruppo Studenti</a>\n", data.Telegram))
 
 	return makeResponseWithText(msg)
 }
