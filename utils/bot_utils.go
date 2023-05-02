4d6a41da1b9a719e890fc5e519c0b9c2fd36a1d1
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2023-05-02 17:57:58 +0200

    Do not break on user not found

--- utils/bot_utils.go
+++ utils/bot_utils.go
@@ -4,16 +4,28 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+func makeUnknownMember(chatConfigWithUser tgbotapi.ChatConfigWithUser) tgbotapi.ChatMember {
+	return tgbotapi.ChatMember{
+		User: &tgbotapi.User{
+			ID:        chatConfigWithUser.UserID,
+			FirstName: "???",
+			LastName:  "???",
+			UserName:  "???",
+		},
+	}
+}
+
 func GetChatMembers(bot *tgbotapi.BotAPI, chatID int64, memberIds []int) ([]tgbotapi.ChatMember, error) {
 	var members []tgbotapi.ChatMember
 
 	for _, id := range memberIds {
-		member, err := bot.GetChatMember(tgbotapi.ChatConfigWithUser{
+		chatConfigWithUser := tgbotapi.ChatConfigWithUser{
 			ChatID: chatID,
 			UserID: id,
-		})
+		}
+		member, err := bot.GetChatMember(chatConfigWithUser)
 		if err != nil {
-			return nil, err
+			member = makeUnknownMember(chatConfigWithUser)
 		}
 		members = append(members, member)
 	}
